741536de3a98a74a9123f3e1595650ee85c840df
saaasssska <fedyev981@gmail.com>
2024-08-21 17:42:51 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+
+var user = {name: 'Дима'};
+
+app.get("/text", function(request, response) {
+    response.send("<h1>Привет, это я!<h1>");
+});
+
+app.get("/json", function(request, response) {
+    response.send("<h1>" + JSON.stringify(user) + "</h1>");
+});
+
+app.get("/hello", function(request, response) {
+    response.send("<h1>Hello, " + request.query.name + "</h1>");
+});
+
+app.get("/tasks/:id", function(request, response) {
+    response.send("<h1>Task id is " + request.params["id"] + "</h1>");
+});
+
+app.listen(3000);
